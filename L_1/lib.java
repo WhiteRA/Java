5ca1fd395c92aca07f3a70ec1b99f0775c703e31
mr.White <WhiteRA_@mail.ru>
2023-06-11 14:37:01 +1000

    +

--- L_1/lib.java
+++ L_1/lib.java
@@ -1,7 +1,5 @@
 package jAVA.L_1;
 
 public class lib {
-    static void crtMass(String[] a){
-        
-    }
+    
 }
--- L_1/t_1.java
+++ L_1/t_1.java
@@ -2,5 +2,24 @@ package jAVA.L_1;
 
 public class t_1 {
     public static void main(String[] args) {
+        class Solution {
+            public String reverseWords(String s) {
+                StringBuilder sb = new StringBuilder();
+                s = s.trim();
+                int n = s.length(), i = n - 1, j = n;
+                while (i >= 0) {
+                    if (s.charAt(i) == ' ') {
+                        sb.append(s.substring(i + 1, j)).append(" ");
+                        while (s.charAt(i) == ' ')
+                            i--;
+                        j = i + 1;
+                    } else
+                        i--;
+                }
+                //Handle the beginning separately
+                sb.append(s.substring(0, j));
+                return sb.toString();
+            }
+        } 
     }
 }
