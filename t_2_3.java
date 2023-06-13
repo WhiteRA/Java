83002217c3b807ba4177647015b48a6ca54f805c
mr.White <WhiteRA_@mail.ru>
2023-06-13 21:10:29 +1000

    2_3

--- t_2_3.java
+++ t_2_3.java
@@ -1,7 +1,20 @@
 package jAVA;
+import java.util.Arrays;
 
 public class t_2_3 {
     public static void main(String[] args) {
         int[] myArray = {1, 22, 33, 44, 5, -5, -3, 23, -7};
+        int index = 0;
+        for (int i = 0; i < myArray.length; i++) {
+            if (myArray[i] >= 10) {
+                index += i;
+            }
+        }
+        for (int i = 0; i < myArray.length; i++) {
+            if (myArray[i] < 0) {
+                myArray[i] = index;
+            }
+        }
+        System.out.println(Arrays.toString(myArray));
     }
 }
