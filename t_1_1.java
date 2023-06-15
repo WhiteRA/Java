ed52f7459eb422836416ddb9fdd50de4e2465c6b
Aleksandr Parhomenko <whitera_@mail.ru>
2023-06-15 15:43:32 +1000

    бесил, закоментил чтоб не бесил

--- t_1_1.java
+++ t_1_1.java
@@ -1,25 +1,26 @@
 package jAVA;
-
-public class t_1_1 {
-    public static void main(String[] args) {
-        class Solution {
-            public String reverseWords(String s) {
-                StringBuilder sb = new StringBuilder();
-                s = s.trim();
-                int n = s.length(), i = n - 1, j = n;
-                while (i >= 0) {
-                    if (s.charAt(i) == ' ') {
-                        sb.append(s.substring(i + 1, j)).append(" ");
-                        while (s.charAt(i) == ' ')
-                            i--;
-                        j = i + 1;
-                    } else
-                        i--;
-                }
-                //Handle the beginning separately
-                sb.append(s.substring(0, j));
-                return sb.toString();
-            }
-        } 
-    }
-}
+/*
+ * public class t_1_1 {
+ * public static void main(String[] args) {
+ * class Solution {
+ * public String reverseWords(String s) {
+ * StringBuilder sb = new StringBuilder();
+ * s = s.trim();
+ * int n = s.length(), i = n - 1, j = n;
+ * while (i >= 0) {
+ * if (s.charAt(i) == ' ') {
+ * sb.append(s.substring(i + 1, j)).append(" ");
+ * while (s.charAt(i) == ' ')
+ * i--;
+ * j = i + 1;
+ * } else
+ * i--;
+ * }
+ * //Handle the beginning separately
+ * sb.append(s.substring(0, j));
+ * return sb.toString();
+ * }
+ * }
+ * }
+ * }
+ */
