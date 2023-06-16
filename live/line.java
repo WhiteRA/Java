60a25f93ce82166e7a7908728df71b259aeab8bf
Aleksandr Parhomenko <whitera_@mail.ru>
2023-06-16 13:46:32 +1000

    +

--- live/line.java
+++ live/line.java
@@ -8,15 +8,7 @@ public class line {
         // int[] arr = { 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 };
         // int[] arr2 = new int[8];
         // int[] mass = { 0, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 };
-        // lib.miror(arr);
-        // lib.fillingUp3(arr2);
-        // lib.upper(mass);
-        // lib.minMax(mass);
 
-        List a = new ArrayList();
-        for (int i = 0; i < 9; i++) {
-            a.add(i, i);
-        }
-        System.out.println(a);
     }
+
 }
