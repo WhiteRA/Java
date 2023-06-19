5554cdcf128d2a905f44a3b5e18c44d5668e44f5
Aleksandr Parhomenko <whitera_@mail.ru>
2023-06-19 14:05:12 +1000

    +

---
+++ T_2/t_2_1.java
@@ -0,0 +1,28 @@
+package T_2;
+
+import java.util.Scanner;
+
+public class t_2_1 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("До какого числа ? ");
+        int num = in.nextInt();
+        int i, j;
+        int k = 0;
+        boolean check;
+        for (i = 2; i < num; i++) {
+            check = true;
+            for (j = 2; j < i; j++) {
+                if ((i % j) == 0) {
+                    check = false;
+                    break;
+                }
+            }
+            if (check) {
+                k += i;
+            }
+        }
+        System.out.println(k);
+    }
+}
---
+++ T_2/t_2_2.java
@@ -0,0 +1,42 @@
+package T_2;
+
+import java.util.Scanner;
+
+public class t_2_2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Размер последовательнисти: ");
+        int num = in.nextInt();
+        int[] myArray = new int[num];
+
+        System.out.print("Начало: ");
+        int beginning = in.nextInt();
+        System.out.println("Убывающая - down/ Возрастающая - up: ");
+        String status = in.next();
+
+        switch (status) {
+            case "down":
+                for (int i = 0; i < myArray.length; i++) {
+                    myArray[i] = beginning;
+                    beginning--;
+                }
+                break;
+            case "up":
+                for (int i = 0; i < myArray.length; i++) {
+                    myArray[i] = beginning;
+                    beginning++;
+                }
+                break;
+            default:
+                break;
+        }
+        for (int i = 0; i < myArray.length; i++) {
+            if (myArray[i] > beginning) {
+
+            } else {
+                System.out.println("Убывающая");
+            }
+        }
+        in.close();
+    }
+}
