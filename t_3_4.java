0f98641419d2fae89ac453b4858ff2e1601ee6c6
Aleksandr Parhomenko <whitera_@mail.ru>
2023-06-19 13:57:24 +1000

    +

---
+++ t_3_4.java
@@ -0,0 +1,31 @@
+package jAVA;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class t_3_4 {
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(53);
+        numbers.add(6);
+        numbers.add(23);
+        numbers.add(14);
+        numbers.add(87);
+
+        // Находим минимальное значение
+        Integer min = Collections.min(numbers);
+        System.out.println("Минимальное значение : " + min);
+
+        // Находим максимальное значение
+        Integer max = Collections.max(numbers);
+        System.out.println("Максимальное значение : " + max);
+
+        // Находим среднее значение
+        double sum = 0;
+        for (Integer number : numbers) {
+            sum += number;
+        }
+        double average = sum / numbers.size();
+        System.out.println("Среднее значение : " + average);
+    }
+}
