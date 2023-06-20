1949f32410b2fee7b805e2fa44f14539497ed8f5
Aleksandr Parhomenko <whitera_@mail.ru>
2023-06-20 12:13:48 +1000

    +

---
+++ T_4/t_4_2.java
@@ -0,0 +1,25 @@
+package jAVA.T_4;
+
+import java.util.Stack;
+
+public class t_4_2 {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else if (stack.empty()) {
+                return false;
+            } else if (c == ')' && stack.peek() == '(') {
+                stack.pop();
+            } else if (c == '}' && stack.peek() == '{') {
+                stack.pop();
+            } else if (c == ']' && stack.peek() == '[') {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
