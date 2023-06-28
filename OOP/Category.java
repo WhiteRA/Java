6f7fc95dfbc52bd12dd799a8026c40f02fae8146
Aleksandr Parhomenko <whitera_@mail.ru>
2023-06-28 16:48:45 +1000

    +

--- OOP/Category.java
+++ OOP/Category.java
@@ -3,4 +3,12 @@ package OOP;
 public class Category {
     private String name;
     private Product[] products;
+
+    public String getname() {
+        return name;
+    }
+
+    public Product[] getProduct() {
+        return products;
+    }
 }
