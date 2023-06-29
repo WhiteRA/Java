850cff74f79f02f4741ecebc0e4867f2fce7e15f
Aleksandr Parhomenko <whitera_@mail.ru>
2023-06-29 17:56:13 +1000

    +

--- OOP/Product.java
+++ OOP/Product.java
@@ -11,6 +11,27 @@ public class Product {
         this.rating = rating;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(double price) {
+        if (price >= 0) {
+            this.price = price;
+        } else {
+            System.out.println(" Цена не может быть меньше 0 ");
+        }
+    }
+
+    public void setRating(double rating) {
+        if (rating >= 0) {
+            this.rating = rating;
+        } else {
+            System.out.println(" Рейтинг не может быть меньше 0 ");
+        }
+
+    }
+
     public String prnProduct() {
         return String.format("Название: %s; Цена: %.02f; Рейтинг: %.01f", name, price, rating);
     }
