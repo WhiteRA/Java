255e58727b96677204aaf6097345d8176658eb00
mr.White <WhiteRA_@mail.ru>
2023-06-30 20:41:17 +1000

    +

--- OOP/Category.java
+++ OOP/Category.java
@@ -1,7 +1,30 @@
 package OOP;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class Category {
-  
+    private String name;
+    List<Product> products;
 
+    public Category(String name) {
+        this.name = name;
+        products = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    public void delProduct(Product product) {
+        products.remove(product);
+    }
 }
