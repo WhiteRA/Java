eee7c1942eb3bbc7cdf82459397ccb69de999ec1
mr.White <WhiteRA_@mail.ru>
2023-06-30 20:47:00 +1000

    +

--- OOP/Basket.java
+++ OOP/Basket.java
@@ -1,5 +1,25 @@
 package OOP;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Basket {
+    private List<Product> products;
+
+    public Basket() {
+        products = new ArrayList<>();
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    public void delProduct(Product product) {
+        products.remove(product);
+    }
 
 }
