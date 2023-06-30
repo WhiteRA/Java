a6905f42410da55d2ec3840929df7ede9e09fa89
mr.White <WhiteRA_@mail.ru>
2023-06-30 20:32:25 +1000

    +

--- OOP/Main.java
+++ OOP/Main.java
@@ -2,5 +2,9 @@ package OOP;
 
 public class Main {
     public static void main(String[] args) {
+        Product product1 = new Product("Смартфон", 1000, 4.5);
+        Product product2 = new Product("Телевизор", 2000, 4.2);
+        Product product3 = new Product("Футболка", 20, 3.8);
+        Product product4 = new Product("Шорты", 30, 4.0);
     }
 }
--- OOP/Product.java
+++ OOP/Product.java
@@ -5,4 +5,40 @@ public class Product {
     private double price;
     private double rating;
 
+    public Product(String name, double price, double rating) {
+        this.name = name;
+        this.price = price;
+        this.rating = rating;
+    }
+
+    public Product() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    
+
 }
