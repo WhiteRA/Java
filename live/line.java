c04f1e1dc215e51f03ea09577033e2fba46c2f00
Aleksandr Parhomenko <whitera_@mail.ru>
2023-07-04 10:38:08 +1000

    +

--- live/line.java
+++ live/line.java
@@ -1,9 +1,28 @@
 package live;
 
+import java.util.ArrayList;
+import java.util.Date;
+
+import javax.xml.crypto.Data;
+
 public class line {
     public static void main(String[] args) {
 
-    
-
+        long start1 = System.currentTimeMillis();
+        int[] a = new int[99999999];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = i;
+        }
+        a[4] = 0;
+        long timeWorkCode1 = System.currentTimeMillis() - start1;
+        System.out.println(timeWorkCode1);
+        System.out.println("-----------------");
+        long start2 = System.currentTimeMillis();
+        ArrayList<Integer> f = new ArrayList<>();
+        for (int i = 0; i < 99999999; i++) {
+            f.add(i);
+        }
+        long timeWorkCode2 = System.currentTimeMillis() - start2;
+        System.out.println(timeWorkCode2);
     }
 }
