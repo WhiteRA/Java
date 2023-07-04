6cde2dc70b609a21b0edcd69c414a4814b2e9bf2
Aleksandr Parhomenko <whitera_@mail.ru>
2023-07-04 10:58:43 +1000

    +

--- live/line.java
+++ live/line.java
@@ -1,28 +1,42 @@
 package live;
 
 import java.util.ArrayList;
-import java.util.Date;
-
-import javax.xml.crypto.Data;
 
 public class line {
     public static void main(String[] args) {
+        /*
+         * long start1 = System.currentTimeMillis();
+         * 
+         * ArrayList<Integer> f = new ArrayList<>();
+         * for (int i = 0; i < 99999999; i++) {
+         * f.add(i);
+         * }
+         * 
+         * long timeWorkCode1 = System.currentTimeMillis() - start1;
+         * System.out.println(timeWorkCode1);
+         * 
+         * System.out.println("-----------------");
+         * 
+         * long start2 = System.currentTimeMillis();
+         * f.remove(4);
+         * f.add(4, null);
+         * long timeWorkCode2 = System.currentTimeMillis() - start2;
+         * System.out.println(timeWorkCode2);
+         */
 
         long start1 = System.currentTimeMillis();
-        int[] a = new int[99999999];
-        for (int i = 0; i < a.length; i++) {
-            a[i] = i;
+        int[] g = new int[99999999];
+        int[] v = new int[g.length - 1];
+        for (int i = 0; i < g.length; i++) {
+            g[i] = i;
         }
-        a[4] = 0;
+        g[4] = 0;
+        for (int i = 0; i < v.length; i++) {
+            if (g[i] != 0) {
+                v[i] = g[i];
+            }
+        }
         long timeWorkCode1 = System.currentTimeMillis() - start1;
         System.out.println(timeWorkCode1);
-        System.out.println("-----------------");
-        long start2 = System.currentTimeMillis();
-        ArrayList<Integer> f = new ArrayList<>();
-        for (int i = 0; i < 99999999; i++) {
-            f.add(i);
-        }
-        long timeWorkCode2 = System.currentTimeMillis() - start2;
-        System.out.println(timeWorkCode2);
     }
 }
