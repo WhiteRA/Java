017bac67758c065f5a8c47297403ae2dc853e41b
mr.White <WhiteRA_@mail.ru>
2023-07-04 23:14:18 +1000

    +

--- live/line.java
+++ live/line.java
@@ -2,6 +2,13 @@ package live;
 
 public class line {
     public static void main(String[] args) {
+        Worker[] a = new Worker[5];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = new Worker("Ivanov Ivan" + i, "Engineer", "ivivan@mailbox.com", "892312312", i * 100,
+                    lib.rnd(40, 50));
 
+        }
+        Worker.getInfoToMass(a);
     }
+
 }
