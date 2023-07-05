05fb54b78311073db13fafba244abc390e2720bc
Aleksandr Parhomenko <whitera_@mail.ru>
2023-07-05 16:43:55 +1000

    +

--- game/Hero.java
+++ game/Hero.java
@@ -1,7 +1,5 @@
 package game;
 
-import java.util.concurrent.TimeUnit;
-
 import live.lib;
 
 public class Hero {
@@ -14,6 +12,8 @@ public class Hero {
                            // способность отражать удары и переносить ране-ния; чем выше уровень
                            // ВЫНОСЛИВОСТИ, тем дольше ты можешь сопротивляться ударам врагов.
     private int luckiness; // Уровень УДАЧЛИВОСТИ показывает, насколько ты везуч.
+    private int win = 0; // для игр на удачу
+    private int lose = 0; // для игр на удачу
 
     public Hero(String name) {
         this.name = name;
@@ -56,12 +56,11 @@ public class Hero {
     }
 
     public void luckCheck() {
-        int win = 0;
-        int lose = 0;
         int badluck = lib.rnd(1, 6) + lib.rnd(1, 6);
         if (luckiness >= badluck) {
             switch (win) {
                 case 1:
+                    this.win++;
                     System.out.println(luckiness);
                     System.out
                             .println("В этот раз тебе повезло " + name
@@ -69,54 +68,65 @@ public class Hero {
                     System.out.println("Помини моё слово, герой...");
                     break;
                 case 2:
+                    win++;
                     System.out.println(luckiness);
                     System.out.println("А ты везучий");
                     break;
                 case 3:
+                    win++;
                     System.out.println(luckiness);
                     System.out.println("Что ?! Опять ?! Не может быть !");
                     break;
                 case 4:
+                    win++;
                     System.out.println(luckiness);
                     System.out.println("Ты жульничаешь !");
                     break;
                 case 5:
+                    win++;
                     System.out.println(luckiness);
                     System.out.println("Тебя Тимора в зад поцеловала чтоли ?!");
                     break;
 
                 default:
+                    System.out.println("Интересно...");
+                    win++;
                     break;
             }
-            win++;
-        } else {
 
+        } else {
             switch (lose) {
                 case 1:
+                    lose++;
                     System.out.println("У теябя выпало - " + badluck);
                     System.out.println("Я же говорил... Ехехехе...");
                     break;
                 case 2:
+                    lose++;
                     System.out.println(luckiness);
                     System.out.println("Сегодня просто не твой день. Ехехехе...");
                     break;
                 case 3:
+                    lose++;
                     System.out.println(luckiness);
                     System.out.println(" Оооо ! Приятель ! Да тебе крупно не везёт ! Ехехехе...");
                     break;
                 case 4:
+                    lose++;
                     System.out.println(luckiness);
                     System.out.println("Ахахахахах ! Да ! Вот это я называю отличным деньком !");
                     break;
                 case 5:
+                    lose++;
                     System.out.println(luckiness);
                     System.out.println("Ой, ой, ой. Посмотри те на него ! Он сейчас расплачется ! Ахахахахах !");
                     break;
 
                 default:
+                    System.out.println("Хм...");
+                    lose++;
                     break;
             }
-            lose++;
         }
     }
 }
