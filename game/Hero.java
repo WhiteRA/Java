9ff161f01e16e4b082ecd1f3e6d9111d8b27c48e
Aleksandr Parhomenko <whitera_@mail.ru>
2023-07-05 13:44:09 +1000

    +

---
+++ game/Hero.java
@@ -0,0 +1,8 @@
+package game;
+
+public class Hero {
+    String name;
+    int skill;
+    int endurance;
+    int luckiness;
+}
--- live/line.java
+++ live/line.java
@@ -2,13 +2,6 @@ package live;
 
 public class line {
     public static void main(String[] args) {
-        Worker[] a = new Worker[10];
-        for (int i = 0; i < a.length; i++) {
-            a[i] = new Worker("Ivanov Ivan" + i, "Engineer", "ivivan@mailbox.com", "892312312", i * 100,
-                    lib.rnd(35, 50));
-            if (a[i].getAge() > 40) {
-                System.out.println(a[i].toString());
-            }
-        }
-    }     
+
+    }
 }
