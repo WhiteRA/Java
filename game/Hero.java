958243f3c8b704563e799feb04bb62e3833899bb
Aleksandr Parhomenko <whitera_@mail.ru>
2023-07-05 13:56:55 +1000

    +

--- game/Hero.java
+++ game/Hero.java
@@ -1,8 +1,46 @@
 package game;
 
+import live.lib;
+
 public class Hero {
-    String name;
-    int skill;
-    int endurance;
-    int luckiness;
+    // Статы не должны быть больше стартовых значений
+    private String name;
+    private int skill;
+    private int endurance;
+    private int luckiness;
+
+    public Hero(String name) {
+        this.name = name;
+        this.skill = lib.rnd(1, 6) + 6;
+        this.endurance = lib.rnd(1, 6) + lib.rnd(1, 6) + 12;
+        this.luckiness = lib.rnd(1, 6);
+    }
+
+    public int getEndurance() {
+        return endurance;
+    }
+
+    public int getLuckiness() {
+        return luckiness;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSkill() {
+        return skill;
+    }
+
+    public void setEndurance(int endurance) {
+        this.endurance = endurance;
+    }
+
+    public void setLuckiness(int luckiness) {
+        this.luckiness = luckiness;
+    }
+
+    public void setSkill(int skill) {
+        this.skill = skill;
+    }
 }
