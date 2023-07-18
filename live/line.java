72836698251313600a2153b2dde7b4dbc6c80136
Aleksandr Parhomenko <whitera_@mail.ru>
2023-07-18 14:25:52 +1000

    +

--- live/line.java
+++ live/line.java
@@ -2,8 +2,10 @@ package live;
 
 public class line {
     public static void main(String[] args) {
-        Calc<Double> a1 = new Calc<>(2.3, 5.1);
+        Calc<Double> a1 = new Calc<Double>(2.3, 2.3);
+        Calc<Integer> a2 = new Calc<Integer>(2, 2);
         a1.slojenie();
+        a2.slojenie();
     }
 
 }
