c08c77fbcc4efc4c32473b713a38403852a81373
mr.White <WhiteRA_@mail.ru>
2023-07-23 16:16:57 +1000

    +

--- OOP/t_5/Main.java
+++ OOP/t_5/Main.java
@@ -2,7 +2,6 @@ package OOP.t_5;
 
 import java.util.Scanner;
 
-// Главный класс
 public class Main {
     public static void main(String[] args) {
         CalculatorModel model = new CalculatorModel();
