189af319054d29181095add1e0a7514aba430845
Aleksandr Parhomenko <whitera_@mail.ru>
2023-07-27 14:27:43 +1000

    +

--- game/Main.java
+++ game/Main.java
@@ -2,12 +2,16 @@ package game;
 
 public class Main {
     public static void main(String[] args) {
-
-        Hero hero1 = new Hero("Konan");
-        // hero1.getInfoOfHero();
-        System.out.println("----------------------------");
-        for (int i = 0; i < 5; i++) {
-            hero1.luckCheck();
+        try {
+            Hero hero1 = new Hero("Konan");
+            // hero1.getInfoOfHero();
+            System.out.println("----------------------------");
+            for (int i = 0; i < 5; i++) {
+                hero1.luckCheck();
+            }
+        } catch (Error e) {
+            System.out.println("Библиотека lib.java ненайдена");
         }
+
     }
 }
